3099aa8e24153efde72d76f1d3328ea28839054e
ZinMarLwin <zimmar07@gmail.com>
2026-05-23 11:49:08 +0900

    Insert data from form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function Item({ item }) {
     return <li>{item.name}</li>;
 }
 
 export default function App() {
+    const inputRef = useRef();
+
     const [data, setData] = useState([
         { id: 3, name: "Bread", done: false },
         { id: 2, name: "Butter", done: true },
@@ -12,16 +14,31 @@ export default function App() {
     ]);
 
     const addItem = () => {
-        setData([{ id: 4, name: "Egg", done: false }, ...data]);
+        const id = data[0].id + 1;
+        const name = inputRef.current.value;
+
+        setData([{ id, name, done: false }, ...data]);
     }
 
-    return (
+return (
         <div>
             <h1>Hello React ({data.length})</h1>
-            <button onClick={addItem}>Add</button>
+            <form onSubmit={e => {
+                e.preventDefault();
+                addItem();
+                e.currentTarget.reset();
+            }}>
+                <input type="text" ref={inputRef} />
+                <button type="submit">Add</button>
+            </form>
             <ul>
                 {data.map(item => {
-                    return <Item key={item.id} item={item} />
+                    return (
+                        <Item
+                            key={item.id}
+                            item={item}
+                        />
+                    );
                 })}
             </ul>
         </div>
